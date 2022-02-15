e53feea928e8ab72d5e1d1128beb0e6bbd0daa0c
Jennifer Guo <jenniferguo100@gmail.com>
2022-02-15 09:04:54 -0500

    get input args

--- randmst.cpp
+++ randmst.cpp
@@ -1,7 +1,30 @@
 #include <stdio.h> 
 #include <stdlib.h>
+#include <math.h>
+#include <string>
 
-int main(){
- printf("Hello World");
-return 0;
+typedef struct
+{
+    int v;
+    int u;
+    float weight;
+}
+edge;
+
+int main(int argc, char *argv[]){
+    //checks that there are command line args
+    if (argc != 5)
+    {
+        printf("Usage: ./randmst 0 numpoints numtrials dimension\n");
+        return 1;
+    }
+
+    int flag = (int) strtol(argv[1], NULL, 10);
+    int numpoints = (int) strtol(argv[2], NULL, 10);
+    int numtrials = (int) strtol(argv[3], NULL, 10);
+    int dimension = (int) strtol(argv[4], NULL, 10);
+
+    printf("%i, %i, %i \n", numpoints, numtrials, dimension);
+
+    return 0;
 }
